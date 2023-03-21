c8e0034d070a895db0451ae519428b87fe0cd839
AishaNabi <aishanabiyeva0@gmail.com>
2023-03-21 18:21:21 +0400

    some changes

--- D-Back for Windows 20230321-170900/reserve.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { } from '../controls/reserveControl.js'
-import requireAuth from '../auth.js'
-
-const r = Router()
-
-r.post('/reserveRoom', requireAuth, reserve)
-
-export default r
